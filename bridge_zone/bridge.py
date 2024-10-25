10064b19c22aed6e4dfad3a2d87e093e61a500a7
Federico Melis <70884674+fedemelis@users.noreply.github.com>
2024-10-25 13:45:25 +0200

    Update bridge.py

--- bridge_zone/bridge.py
+++ bridge_zone/bridge.py
@@ -1,8 +1,17 @@
 # from SX127x.LoRa import LoRa
 import SessionHTTP as Http
 
-def sendInitData(data):
+
+def sendInitData(zone, data):
     # send data to the microcontroller using LoRa
+
+    """
+
+    :param zone: the unique identifier for microcontroller
+    :param data: the JSON data to send
+    :return: return True if no error occurs, False otherwise
+    """
+
     pass
 
 
@@ -22,22 +31,45 @@ def fetchInitData(zone, url):
         print('Data:')
         for key, value in data.items():
             print(f'{key}: {value}\n')
-        return sendInitData(data)
-
+        return data
 
 def handleMessage():
     pass
+
 
 def loop():
     while True:
         # if there is a message
         handleMessage()
 
+
 def startBridge():
+    initList = [False for i in range(0, 255)]
+
     """"
     we will stay in this loop until all the microcontrollers have contacted the bridge
     """
-    pass
+    while initList.count(False) > 0:
+        # if channel.available > 0
+        # suppose we riceved a dictionary from microcontroller
+        data = {
+            'head': {
+                'start': 0xff,
+                'type': 0xAA,  # stands for initialization request packet
+                'size': 1,
+            },
+            'zone': 0,
+            'end': 0xFE
+        }
+
+        if data['head']['type'] == 0xAA:
+            zone = data['zone']
+            if zone and not initList[zone]:
+                data = fetchInitData(zone, url='http://esempio.com')
+                if sendInitData(zone, data):
+                    initList[zone] = True
+
+
 
 
 def setup():
